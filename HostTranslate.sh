c11ebf6847123c1069e9859af3e1428a5135c00d
Mike Himbeault <mike@forethought.solutions>
2021-11-30 14:25:36 -0600

    Update HostTranslate.sh with new stuff

--- HostTranslate.sh
+++ HostTranslate.sh
@@ -8,31 +8,42 @@ rate="$2"
 function get_incremental_upload
 {
     hash="$1"
-    uploaded=$(cat "$hash.json" | jq .uploaded_at_last_update)
-    last_update_time=$(cat "$hash.json" | jq .last_update_time)
+    peer_id="$2"
+    uploaded=$(cat "$hash.json" | jq ".[\"$peer_id\"].uploaded_at_last_update")
+    last_update_time=$(cat "$hash.json" | jq ".[\"$peer_id\"].last_update_time")
     seconds="$[`date -u +%s`-last_update_time]"
-    incremental_upload=$(echo "scale=40;$2*(0.8+0.4*(`date +%N | head -c5`/100000))*$seconds" | \
+    incremental_upload=$(echo "scale=40;$3*(0.8+0.4*(`date +%N | head -c5`/100000))*$seconds" | \
         bc | cut -d '.' -f1)
     echo "$[uploaded+incremental_upload]"
 }
 
 cat - | while read line
 do
-    hash=$(echo "$line" | grep -o 'hash=[^&"]*' | cut -d '=' -f2 | tr -d '%')
-    reported_upload=$(echo "$line" | grep -o "uploaded=[0-9]*" | cut -d '=' -f2)
+    hash=$(echo "$line" | grep -o '[&?]info_hash=[^&"]*' | cut -d '=' -f2 | tr -d '%')
+    reported_upload=$(echo "$line" | grep -o '[?&]uploaded=[0-9]*' | cut -d '=' -f2)
+    peer_id=$(echo "$line" | grep -o '[?&]peer_id=[^&"]*' | cut -d '=' -f2)
 
     if [ "$hash" != "" ]
     then
         if [ ! -f "$hash.json" ]
         then
-            echo "{\"uploaded_at_last_update\": $reported_upload, \"last_update_time\": `date -u +%s`, \"start_time\": `date -u +%s`}" > "$hash.json"
+            echo "{\"$peer_id\": {\"uploaded_at_last_update\": $reported_upload, \"last_update_time\": `date -u +%s`, \"start_time\": `date -u +%s`}}" > "$hash.json"
         fi
 
-        new_up_bytes=$(get_incremental_upload "$hash" "$rate")
+        peer_id_exists=$(cat "$hash.json" | jq ". | has(\"$peer_id\")")
+
+        if [ "$peer_id_exists" == "false" ]
+        then
+            cat "$hash.json" | \
+            jq ".[\"$peer_id\"] = {\"uploaded_at_last_update\": $reported_upload, \"last_update_time\": `date -u +%s`, \"start_time\": `date -u +%s`}" | \
+            sponge "$hash.json"
+        fi
+
+        new_up_bytes=$(get_incremental_upload "$hash" "$peer_id" "$rate")
         echo "NEW_UP_BYTES $hash $new_up_bytes" >&2
 
         cat "$hash.json" | \
-            jq ".uploaded_at_last_update = $new_up_bytes | .last_update_time = `date -u +%s`" | \
+            jq ".[\"$peer_id\"].uploaded_at_last_update = $new_up_bytes | .[\"$peer_id\"].last_update_time = `date -u +%s`" | \
             sponge "$hash.json"
 
         echo "$line" | \
